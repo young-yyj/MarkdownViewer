c412f31b444af7ee3e616e655e24c6a3caab746f
young-yyj <49195621+young-yyj@users.noreply.github.com>
2026-04-28 21:29:18 +0800

    feat: add WPF value converters for bool-to-visibility and inverse

---
+++ Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MarkdownViewer.Converters;
+
+public class BoolToVisibilityConverter : IValueConverter
+{
+    public bool Invert { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var boolValue = value is bool b && b;
+        if (Invert) boolValue = !boolValue;
+        return boolValue ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is Visibility v && v == Visibility.Visible ? !Invert : Invert;
+    }
+}
---
+++ Converters/InverseBoolConverter.cs
@@ -0,0 +1,17 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MarkdownViewer.Converters;
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is bool b && !b;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is bool b && !b;
+    }
+}
